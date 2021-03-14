7f1a4f90a69f1e2e736804bb9285e0392d84658f
Jack-Moon628 <bright_96_628@outlook.com>
2021-03-14 05:43:54 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Menu, tabListBehavior, Tree } from '@fluentui/react-northstar';
+import { TriangleDownIcon, TriangleEndIcon } from '@fluentui/react-icons-northstar';
+import { LeftSideBar, ContentContainer } from './styles';
+import { ReactMegaMenu } from "react-mega-menu";
+
+interface MenuItem { key: string, content: string };
+
+interface Type<SidebarItem extends {}> {
+  id: string;
+  title: string;
+  items: SidebarItem[],
+  field: new<T extends Type<any>>(type: T) => { };
+}
+
+const navItems:MenuItem[] = [
+  {
+    key: 'dashbord',
+    content: 'Dashbord',
+  },
+  {
+    key: 'settings',
+    content: 'Settings',
+  },
+];
+
+interface Item {
+  id: string,
+  title: string,
+  items?: Item[],
+}
+
+const sidebarItems: Item[] = [
+  {
+    id: 'tree-title-customization-item-1',
+    title: 'one',
+    items: [
+      {
+        id: 'tree-title-customization-item-2',
+        title: 'one one',
+        items: [
+          {
+            id: 'tree-title-customization-item-3',
+            title: 'one one one',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 'tree-title-customization-item-4',
+    title: 'two',
+    items: [
+      {
+        id: 'tree-title-customization-item-5',
+        title: 'two one',
+      },
+    ],
+  },
+]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Menu
+        defaultActiveIndex={0}
+        items={navItems}
+        underlined
+        primary
+        accessibility={tabListBehavior}
+        aria-label="Today's events"
+      />
+      <LeftSideBar>
+        {/* <Tree aria-label="Custom title" items={sidebarItems} renderItemTitle={titleRenderer} /> */}
+        <Tree
+          defaultSelectedItemIds={['tree-item-122', 'tree-item-123']}
+          selectable
+          aria-label="Multi Select"
+          items={sidebarItems}
+        />
+      </LeftSideBar>
+      <ContentContainer>
+
+      </ContentContainer>
     </div>
   );
 }
---
+++ src/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const LeftSideBar = styled.div.attrs({
+})`
+  width: 30%;
+`
+export const ContentContainer = styled.div.attrs({
+})`
+  width: 70%;
+`
